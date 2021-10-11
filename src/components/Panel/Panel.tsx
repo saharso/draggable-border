d77a50c3638f02f267ce5f267e44b0b1313ae0ec
Sahar Soffer <saharsoffer@Sahars-MacBook-Pro.local>
2021-10-11 12:06:03 +0300

    add another example to demo

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -11,6 +11,7 @@ const Panel: React.FunctionComponent<PanelProps> = ({}) => {
         <DraggableBorder target={'#panelSidebar'} horizontal={true}/>
         <DraggableBorder target={'#panelSidebar3'} horizontal={false}/>
         <DraggableBorder target={'#panelSidebar4'} horizontal={false}/>
+        <DraggableBorder target={'#panelSidebar5'} horizontal={true}/>
 
         <article className={'Panel'}>
 
@@ -26,7 +27,10 @@ const Panel: React.FunctionComponent<PanelProps> = ({}) => {
             <main className={'Panel__main l-column'}>
                 <div className={'Panel__main__top l-stretch'}></div>
 
-                <section id="panelSidebar2" className={'Panel__main__bottom'}></section>
+                <section id="panelSidebar2" className={'Panel__main__bottom'}>
+                    <section id="panelSidebar5" style={{width: '12em'}}></section>
+                    <div></div>
+                </section>
 
                 <section id="panelSidebar4" className={'Panel__main__bottom'}></section>
 
